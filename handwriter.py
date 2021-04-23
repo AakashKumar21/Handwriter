d55a0e8be04ccea5c276ffee727678d78dfea3cb
Aakash <aakashk.kvjp58@gmail.com>
2021-04-23 19:48:23 +0530

    refactored

--- handwriter.py
+++ handwriter.py
@@ -4,58 +4,76 @@ from math import log, sin
 from os import listdir, curdir
 from config import *
 
-# Font and Page
-page_res = {"normal": (800, 1128), "high": (1200, 1692)}
-line_gap_min = FONT_SIZE + LINE_GAP
-line_gap_entropy_max = LINE_GAP_ENTROPY_PC - 5
-current_page = 0
-# line_gap_entropy_max = int(line_gap_entropy_percent/100) * font_size
+PAGE_RES = {"normal": (800, 1128), "high": (1200, 1692)}
+
+
+# Check entropy and assing 1 if less or equal to 0
+def _sanitize_entropy_var():  
+    global LINE_GAP_ENTROPY_MAX
+    global LINE_SLOPE_ENTROPY_MAX
+    global LETTER_ENTROPY_MAX
+    global WORD_ENTROPY_MAX 
+    if LINE_GAP_ENTROPY_MAX <= 0:
+        LINE_GAP_ENTROPY_MAX = 1
+    if LINE_SLOPE_ENTROPY_MAX <= 0:
+        LINE_SLOPE_ENTROPY_MAX = 1
+    if LETTER_ENTROPY_MAX <= 0:
+        LETTER_ENTROPY_MAX = 1
+    if WORD_ENTROPY_MAX <= 0:
+        WORD_ENTROPY_MAX = 1
+
 
 # Line Related
-line_slope_entropy_max = int(
-    (LINE_SLOPE_ENTROPY_PC/100) * FONT_SIZE/LINE_SLOPE_ENTROPY_FONT_DIV)
-
-# Letter and Word Entropy
-letter_entropy_max = int((LETTER_ENTROPY_PC/100) * FONT_SIZE)
-word_entropy_max = int((WORD_ENTROPY_PC/100) * FONT_SIZE)
-
-# Check entropy
-if line_gap_entropy_max <= 0:
-    line_gap_entropy_max = 1
-if line_slope_entropy_max <= 0:
-    line_slope_entropy_max = 1
-if letter_entropy_max <= 0:
-    letter_entropy_max = 1
-if word_entropy_max <= 0:
-    word_entropy_max = 1
-
-# Starting pos
-pos_x = MARGIN_LEFT
-pos_y = MARGIN_TOP
-
-# Counters
-count_page = 0
-count_lines = 1
-
-# Random Noise
-max_dot_rad = 10
-num_of_dots = 100
-
-# Background
-background_entropy = 4
-path = "images\\random_bg\\"
-
-# File Names
-filename = ""
-img_list = []
+def set_max_entropy_values():
+    global LINE_SLOPE_ENTROPY_MAX
+    global LETTER_ENTROPY_MAX
+    global WORD_ENTROPY_MAX
+    global LINE_GAP_ENTROPY_MAX
+    global LINE_GAP_MIN
+    # Line entropy
+    LINE_GAP_ENTROPY_MAX = LINE_GAP_ENTROPY_PC - 5
+    LINE_SLOPE_ENTROPY_MAX = int(
+        (LINE_SLOPE_ENTROPY_PC/100) * FONT_SIZE/LINE_SLOPE_ENTROPY_FONT_DIV)
+    # Letter and Word Entropy
+    LETTER_ENTROPY_MAX = int((LETTER_ENTROPY_PC/100) * FONT_SIZE)
+    WORD_ENTROPY_MAX = int((WORD_ENTROPY_PC/100) * FONT_SIZE)
+    LINE_GAP_MIN = FONT_SIZE + LINE_GAP
+    # Sanitise values
+    _sanitize_entropy_var()
 
 
-def create_bg():
+def reset_xy_pos():
+    global pos_x, pos_y
+    # Starting pos
+    pos_x = MARGIN_LEFT
+    pos_y = MARGIN_TOP
+
+
+def set_initial_values():
+    global count_lines, count_page
+    global background_entropy
+    global current_filename, current_img_lst, current_page
+    global BG_PATH, FONT, FONT_SIZE
+    # Counters
+    count_page = 0
+    count_lines = 1
+    current_page = 0
+    # Background
+    background_entropy = 4
+    BG_PATH = "images\\random_bg\\"
+    FONT = ImageFont.truetype(font_name, FONT_SIZE)
+    # File Names
+    current_filename = ""
+    current_img_lst = []
+    reset_xy_pos()
+
+
+def _create_bg():
     global img
     # Get file names
     bg_parts = []
     if background_entropy == 4:
-        all_parts = listdir(path)
+        all_parts = listdir(BG_PATH)
         for part in all_parts:
             if "cross_4" in part:
                 bg_parts.append(part)
@@ -65,19 +83,12 @@ def create_bg():
     width, height = 0, 0
     for row in range(background_entropy*background_entropy):
         for col in range(4):
-            bg_img = Image.open(path + bg_parts[randrange(len(bg_parts))])
+            bg_img = Image.open(BG_PATH + bg_parts[randrange(len(bg_parts))])
             img.paste(bg_img, (pos_x, pos_y))
             width, height = bg_img.size
             pos_x += width
         pos_x = 0
         pos_y += height
-
-
-# Create Objects and page
-font = ImageFont.truetype(font_name, FONT_SIZE)
-img = Image.new('RGB', page_res["normal"], color=PAGE_COLOR)
-draw = ImageDraw.Draw(img)
-create_bg()
 
 
 def create_page():
@@ -86,28 +97,26 @@ def create_page():
     global img
     global draw
     global count_page
-    img = Image.new('RGB', page_res["normal"], color=PAGE_COLOR)
+    img = Image.new('RGB', PAGE_RES["normal"], color=PAGE_COLOR)
     draw = ImageDraw.Draw(img)
-    create_bg()
+    _create_bg()
     pos_x = MARGIN_LEFT
     pos_y = MARGIN_TOP
     count_page += 1
 
 
 def start_writing(_filename):
+    global current_filename
     f = open(_filename, 'r')
     file_lines_list = f.read().split('\n')  # GET LINES
-    for line in file_lines_list:
-        # print("LINE: ")
-        for word in line.split():
-            # print("WORD: ")
+    for line in file_lines_list:            # For each lines
+        for word in line.split():           # For each words
             write_word(word)
         insert_new_line()
-    save_image(filename + str(current_page))
+    save_image(current_filename + str(current_page))
+
 
 # returns a y point for x input
-
-
 def get_ypos(_x):
     if LINE_SLANTNNG_STLYE == 0:
         y = 0
@@ -126,39 +135,29 @@ def insert_new_line():
     global pos_x
     global pos_y
     global count_lines
-    pos_y += line_gap_min + \
-        randrange(-line_gap_entropy_max, line_gap_entropy_max)
+    pos_y += LINE_GAP_MIN + \
+        randrange(-LINE_GAP_ENTROPY_MAX, LINE_GAP_ENTROPY_MAX)
     pos_x = MARGIN_LEFT
     count_lines += 1
 
 
 def print_info():
     print("HANDWRITER")
-    print("Page Res: ", page_res)
+    print("Page Res: ", PAGE_RES)
     print("page_color:", PAGE_COLOR)
     print("text_color:", TEXT_COLOR)
     print("word_space:", WORD_SPACE)
     print("letter_space:", LETTER_SPACE)
-    print("letter_entropy_max:", letter_entropy_max)
-    print("word_entropy_max:", word_entropy_max)
+    print("letter_entropy_max:", LETTER_ENTROPY_MAX)
+    print("word_entropy_max:", WORD_ENTROPY_MAX)
     print("line_slope:", LINE_SLOPE)
     print("line_var_a:", LINE_VAR_A)
     print("line_sin_para_div", LINE_SIN_DIV)
     print("line_slope_entropy_percent:", LINE_SLOPE_ENTROPY_PC)
     print("line_slope_entropy_font_div:", LINE_SLOPE_ENTROPY_FONT_DIV)
-    print("line_slope_entropy_max:", line_slope_entropy_max)
+    print("line_slope_entropy_max:", LINE_SLOPE_ENTROPY_MAX)
     print("line_slope_x_power", LINE_SLOPE_X_POWER)
     # print("",)
-
-
-def test1():
-    print_info()
-    test_str = "A pixel font is a font that looks crisp/aliased on the screen; a pixel is either black (or any other color) or white (or any other color); not shaded in between - maximum contrast. A bitmap font is by definition a pixel font, but an outline font can be a pixel font as well."
-    test_str = test_str.split()
-    for word in test_str:
-        write_word(word)
-    save_image(filename + str(current_page))
-    # Writeline test
 
 
 def write_word(_word):
@@ -166,56 +165,60 @@ def write_word(_word):
     global pos_x
     global pos_y
     global current_page
+    global WORD_ENTROPY_MAX
+    global draw
 
     # Check if line if full
-    width, height = draw.textsize(_word, font=font)
+    width, height = draw.textsize(_word, font=FONT)
     # Width full go to next line
-    if (width + pos_x + MARGIN_RIGHT) >= page_res["normal"][0]:
+    if (width + pos_x + MARGIN_RIGHT) >= PAGE_RES["normal"][0]:
         insert_new_line()
         # print("LINE FULL")
 
     # Check if page is full
-    if (pos_y + MARGIN_BOTTOM) >= page_res["normal"][1]:
-        save_image(filename + str(current_page))
+    if (pos_y + MARGIN_BOTTOM) >= PAGE_RES["normal"][1]:
+        save_image(current_filename + str(current_page))
         current_page += 1
         create_page()
         # print("PAGE FULL")
 
-    word_ypos_entropy = randrange(-word_entropy_max,
-                                  word_entropy_max) + log(pos_y) + get_ypos(pos_x)
+    word_ypos_entropy = randrange(-WORD_ENTROPY_MAX,
+                                  WORD_ENTROPY_MAX) + log(pos_y) + get_ypos(pos_x)
     # Write the word
     for letter in _word:
-        letter_height_entropy = randrange(-letter_entropy_max,
-                                          letter_entropy_max)
+        letter_height_entropy = randrange(-LETTER_ENTROPY_MAX,
+                                          LETTER_ENTROPY_MAX)
         total_ent = word_ypos_entropy + letter_height_entropy
-        width, height = draw.textsize(letter, font=font)
+        width, height = draw.textsize(letter, font=FONT)
         draw.text((pos_x, pos_y + word_ypos_entropy + total_ent),
-                  letter, font=font, fill=TEXT_COLOR)
+                  letter, font=FONT, fill=TEXT_COLOR)
         pos_x += width + LETTER_SPACE
         # pos_y += height
     # Give space after word
     space = ' '*WORD_SPACE
-    draw.text((pos_x, pos_y), space, font=font, fill=TEXT_COLOR)
-    width, height = draw.textsize(space, font=font)
+    draw.text((pos_x, pos_y), space, font=FONT, fill=TEXT_COLOR)
+    width, height = draw.textsize(space, font=FONT)
     pos_x += width
     # pos_y += height
     return 0
 
 
 def save_image(_filename):
+    global current_img_lst
     img_dir = 'images/'
     img.save(img_dir + _filename + '.png')
-    img_list.append(img_dir + filename + str(current_page) + '.png')
+    current_img_lst.append(img_dir + current_filename + str(current_page) + '.png')
 
 
 def save_pdf(_filename):
+    global current_img_lst
     print("Creating PDF")
     image_object_list = []
-    print("Adding Image:", img_list[0])
-    img = Image.open(img_list[0])  # will hold a single image object
-    img_list.pop(0)
+    print("Adding Image:", current_img_lst[0])
+    img = Image.open(current_img_lst[0])  # will hold a single image object
+    current_img_lst.pop(0)
 
-    for fname in img_list:
+    for fname in current_img_lst:
         print("Adding Image:", fname)
         image_object_list.append(Image.open(fname))
     img.save('pdf/' + _filename + '.pdf', save_all=True,
@@ -225,7 +228,7 @@ def save_pdf(_filename):
 def get_texts_and_write():
     # Get file names
     global filename
-    global img_list
+    global current_img_lst
     text_files_list = []
     all_files = listdir(curdir)
     print("Text Files Found:")
@@ -236,13 +239,18 @@ def get_texts_and_write():
 
     # For each file create images
     for textfile in text_files_list:
-        img_list.clear()
-        filename = textfile.replace(".txt", '') + '_'
+        current_img_lst.clear()
+        current_filename = textfile.replace(".txt", '') + '_'
         start_writing(textfile)
-        save_pdf(filename)
+        save_pdf(current_filename)
+        set_initial_values()
 
+def main():
+    set_initial_values()
+    set_max_entropy_values()
+    create_page()
+    get_texts_and_write()
+    print("Created: Pages:{}, Lines:{}".format(str(count_page), str(count_lines)))
+    
 
-create_page()
-get_texts_and_write()
-# test1()
-print("Created: Pages:{}, Lines:{}".format(str(count_page), str(count_lines)))
+main()
